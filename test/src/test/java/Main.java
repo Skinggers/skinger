79379e3db45c971731ac379a54c80dee07cfb34c
skinger <skingerhi@gmail.com>
2023-11-17 10:45:51 +0800

    第一次提交

---
+++ test/src/test/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*** @Author：Skinger
+ * @Date 2023/10/21 15:47
+ **/
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        String[] split = s.split(",");
+        int y= Integer.parseInt(split[0]);
+        int m= Integer.parseInt(split[1]);
+        int d= Integer.parseInt(split[2]);
+        switch (y%4)
+
+    }
+}
+
+
